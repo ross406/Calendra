4c12d0c94024d8b1ee7b195cb70a1cbf087e26cd
ross406 <roshansharma406@gmail.com>
2025-07-01 09:32:49 +0530

    Ready for deployment

--- app/(main)/(public)/book/page.tsx
+++ app/(main)/(public)/book/page.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+'use client'
 
-function page() {
-  return (
-    <div>
-      PAGE
-    </div>
-  )
+import Loading from "@/components/Loading"
+import { useUser } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
+
+export default function PublicPage() {
+  const { user, isLoaded } = useUser()  // Using `isLoaded` to check if user data is available
+
+  if (!isLoaded) {
+    // Display loading until user data is loaded
+    return <Loading />
+  }
+
+  if (!user) {
+    // Redirect to login if no user is found
+    return redirect('/login')
+  }
+
+  // Once user is available, redirect to the booking page [Public Profile Page]
+  return redirect(`/book/${user.id}`)
 }
-
-export default page
